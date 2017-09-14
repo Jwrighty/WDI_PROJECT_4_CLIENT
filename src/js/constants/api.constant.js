fb2f4770f3bcf6fa8fb1d6e28a68b468b34cb24c
Jwrighty <jason.wrighty@gmail.com>
2017-09-14 14:55:49 +0100

    update API constant

--- src/js/constants/api.constant.js
+++ src/js/constants/api.constant.js
@@ -1,3 +1,4 @@
 angular
   .module('GoGenki')
-  .constant('API', `http://localhost:3000/api`);
+  // .constant('API', `http://localhost:3000/api`);
+  .constant('API', `https://nihongo-api.herokuapp.com/api`);
